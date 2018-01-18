30f0bacf0f6be6347ab8d38d2fed2d7691ac4f59
Will Thames <will.thames@xvt.com.au>
2018-01-18 14:18:41 +1000

    Remove unused pip install from build script

--- build.sh
+++ build.sh
@@ -4,9 +4,5 @@ rm -f ecs-event-monitor.zip
 rm -rf build
 mkdir build
 cp -a src/* build
-# https://github.com/pypa/pip/issues/3826
-# --system is required as otherwise defaults to --user
-# which clashes with -t (--target)
-pip install --system -r build/requirements.txt -t build
 (cd build; zip -r ../ecs-event-monitor.zip *)
 ls -l ecs-event-monitor.zip
