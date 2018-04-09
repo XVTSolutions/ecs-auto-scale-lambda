e888d286bdcb6b75db0fef335349071410c89ac4
Steve Kieu <steve.kieu@xvt.com.au>
2018-04-09 17:27:36 +1000

    remove-dash-from-lambda-handler
    AWS says: "errorMessage": "Unable to import module 'ecs-auto-scale'"
    
    aws lambda will try to import the module name using the module part of the
    param: 'handler'. This should be python name qualified and dash '-' is not.
    
    This hopefully fix that error.

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -e
 
-rm -f ecs-auto-scale.zip
+rm -f ecs_auto_scale.zip
 rm -rf build
 mkdir build
 cp -a src/* build
-(cd build; zip -r ../ecs-auto-scale.zip *)
-ls -l ecs-auto-scale.zip
+(cd build; zip -r ../ecs_auto_scale.zip *)
+ls -l ecs_auto_scale.zip
--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@
 read AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY AWS_SESSION_TOKEN < <(python ~/assumerole.py arn:aws:iam::${bamboo_build_account_id}:role/bamboo_s3_push "${bamboo.buildResultKey}")
 export AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY AWS_SESSION_TOKEN
 
-aws s3 cp ecs-auto-scale.zip s3://${bamboo_build_account_name}-infra-build/
-aws lambda update-function-code --function-name ${bamboo_cluster_name}-ecs-auto-scale --s3-bucket ${bamboo_build_account_name}-infra-build --s3-key ecs-auto-scale.zip
+aws s3 cp ecs_auto_scale.zip s3://${bamboo_build_account_name}-infra-build/
+aws lambda update-function-code --function-name ${bamboo_cluster_name}-ecs_auto_scale --s3-bucket ${bamboo_build_account_name}-infra-build --s3-key ecs_auto_scale.zip
